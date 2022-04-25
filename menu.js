074a5e7e7b449c292383198dff213845295e1878
viveroa2291 <viveroa2291@uwec.edu>
2022-04-25 13:09:40 -0500

    Update
    Updated the menu and got the prices and quantity to work on javascript.

--- menu.js
+++ menu.js
@@ -1,9 +1,11 @@
+
+/*
 var x = 0;
 
 document.getElementById('output-area').innerHTML = x;
 
 function button1() {
-  document.getElementById('output-area').innerHTML = ++x;
+   document.getElementById('output-area').innerHTML = ++x;
 }
 
 function button2() {
@@ -13,6 +15,64 @@ if(x < 2)
   }
   document.getElementById('output-area').innerHTML = --x;
   
+}
+*/
+function vegetarian() {
+   
+    var x = 1;
+
+    document.getElementById('output-area').innerHTML = x;
+    
+    const previousButton = document.getElementById('previous-button'); // The back button
+    const nextButton = document.getElementById('next-button'); // The next button
+    const foodType = document.getElementById('food-type'); // header title
+    const images = document.getElementById('images');
+    const descriptionDiv = document.getElementById('descriptionBody');
+    const price = document.getElementById('price');
+    const plusButton = document.getElementById('plus-button');
+    const minusButton = document.getElementById('minus-button');
+
+    plusButton.addEventListener('click', () => document.getElementById('output-area').innerHTML = ++x);
+    minusButton.addEventListener('click', () => document.getElementById('output-area').innerHTML = --x);
+
+    const headerTitles = ["Vegetarian Fajita", "Burritos De Frijol", "Burrito Vegetariano"];
+    const prices = [14.99, 12.99, 11.99];
+    const headerDescritpions = ["Grilled bell peppers, onions, tomato, potatoes, carrots, zucchini, yellow squash and mushrooms. <br> Served with rice, beans and lettuce.",
+     "Two bean burritos topped with cheese sauce. <br> Served with rice and lettuce.", 
+     "A large flour tortilla filled with grilled bell peppers, onions, potatoes, zucchini, carrots, yellow squash, tomatoes and mushrooms. <br> Topped with cheese sauce and served with rice and lettuce."];
+
+    const imageSource = ["images/universal.jpeg", "images/acapulcoMe.jpeg", "images/alamo.jpeg", "images/arches.jpeg", "images/maggieDaley.jpeg", "images/bucs.jpeg"];
+    let s = 0;
+
+    function advance(delta) {
+        s = (s + delta + headerTitles.length) % headerTitles.length;
+        descriptionDiv.innerHTML = headerDescritpions[s];
+        foodType.innerHTML = headerTitles[s];
+        images.src = imageSource[s];
+        minusButton.addEventListener('click', () => { 
+            if(x < 1) // Number cannot go under 0
+            { 
+                x = 1;
+                price.innerHTML = "$ " + prices[s] * x; 
+                document.getElementById('output-area').innerHTML = x;
+            }
+            else {
+                price.innerHTML = "$ " + prices[s] * x;
+            }     
+        })
+        plusButton.addEventListener('click', () => price.innerHTML = "$ " + prices[s] * x); // If clicked, it will update the price 
+     //   minusButton.addEventListener('click', () => price.innerHTML = "$ " + prices[s] * x); // If clicked, it will update the price
+        price.innerHTML = "$ " + prices[s] * x; 
+    }
+    previousButton.addEventListener('click', () => x = 1); 
+    nextButton.addEventListener('click', () => x = 1);
+
+    previousButton.addEventListener('click', () => advance(-1));
+    previousButton.addEventListener('click', () =>  document.getElementById('output-area').innerHTML = x);
+    nextButton.addEventListener('click', () => advance(1));
+    nextButton.addEventListener('click', () =>  document.getElementById('output-area').innerHTML = x);
+
+    advance(0);  
 }
 function tacos() {
     const previousButton = document.getElementById('previous-button'); // The back button
