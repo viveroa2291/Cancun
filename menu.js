15039df13c7e1e0d2362aa1130568b39689249b4
viveroa2291 <viveroa2291@uwec.edu>
2022-05-03 18:01:31 -0500

    Update
    Got the hamburger when it is in the mobile version. Still have lots of work to do, but took me a while to get that working.

--- menu.js
+++ menu.js
@@ -1,3 +1,9 @@
+function toggleNav() {    
+    let elements = document.getElementsByClassName('top-nav');
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle('show');
+    }
+}
 function vegetarian() {
    
     var x = 1;
