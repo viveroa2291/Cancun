0b6087c442879a84088ced034c2d6afcc628bb81
viveroa2291 <viveroa2291@uwec.edu>
2024-06-25 23:04:58 -0500

    Condensing
    Condensing folders to organize better

--- menu.js
+++ menu.js
@@ -14,7 +14,7 @@ const vegeterian = new MenuSelection(["Vegetarian"],
 [14.99, 12.99, 11.99], 
 ["Grilled bell peppers, onions, tomato, potatoes, carrots, zucchini, yellow squash and mushrooms. <br> Served with rice, beans and lettuce.", "Two bean burritos topped with cheese sauce. <br> Served with rice and lettuce.", "A large flour tortilla filled with grilled bell peppers, onions, potatoes, zucchini, carrots, yellow squash, tomatoes and mushrooms. <br> Topped with cheese sauce and served with rice and lettuce."],
 ["* Tomatoes, sour cream and pico de gallo are complementary upon request. <hr width='25%'>"],
-["vegetarian/vegetarianFajitas.png", "vegetarian/burritoDeFrijole.png", "vegetarian/burritoVegetariano.jpeg"]);
+["menuImage/vegetarian/vegetarianFajitas.png", "menuImage/vegetarian/burritoDeFrijole.png", "menuImage/vegetarian/burritoVegetariano.jpeg"]);
 
 const appetizersClass = new MenuSelection(["Appetizers"],
 ["Queso Fundido", "Mozzarella Sticks", "Cheese Fries", "Chicken Tenders", "Cancun Sampler", "Ceviche De Camaron"],
@@ -24,7 +24,7 @@ const appetizersClass = new MenuSelection(["Appetizers"],
     "A combo of cheese sticks, chicken tenders, chicken quesadilla & queso fundido. <br> Served with lettuce and guacamole.",
     "Fresh shrimp chopped and marinated with lime, tomato, red onions, cilantro, and diced jalapenos. <br> Served with advocado slices, chips and crackers."],
 [""],
-["appetizers/quesoFundido.jpeg", "appetizers/mozzarellaSticks.jpeg", "appetizers/cheeseFries.jpeg", "appetizers/chickenTenders.jpeg", "appetizers/cancunSampler.png", "appetizers/cevicheDeCamaron.jpeg"]);
+["menuImage/appetizers/quesoFundido.jpeg", "menuImage/appetizers/mozzarellaSticks.jpeg", "menuImage/appetizers/cheeseFries.jpeg", "menuImage/appetizers/chickenTenders.jpeg", "menuImage/appetizers/cancunSampler.png", "menuImage/appetizers/cevicheDeCamaron.jpeg"]);
 
 const nachosClass = new MenuSelection(["Nachos"],
 ["Nachos De Mesa", "Nachos Fajitas", "Nachos With Cheese", "Nachos Supreme"],
@@ -32,8 +32,8 @@ const nachosClass = new MenuSelection(["Nachos"],
 ["Cheese nachos with beef, chicken, and beans. <br> Topped with lettuce and shredded cheese.",
     "Your choice of Grilled Chicken or Steak with sauteed onions and bell peppers.", " ", 
     "Your choice of ground Beef or Chicken, topped with lettuce.", " "],
-    ["* All nachos topped with shredded cheese and cheese sauce. * <hr width='25%'>"],
-    ["nachos/nachosDeMesa.png", "nachos/nachoFajitas.jpeg", "nachos/nachosWithCheese.jpeg", "nachos/nachosSupreme.jpeg"]
+["* All nachos topped with shredded cheese and cheese sauce. * <hr width='25%'>"],
+["menuImage/nachos/nachosDeMesa.png", "menuImage/nachos/nachoFajitas.jpeg", "menuImage/nachos/nachosWithCheese.jpeg", "menuImage/nachos/nachosSupreme.jpeg"]
 );
 
 const saladsClass = new MenuSelection(["Salads"],
@@ -45,7 +45,7 @@ const saladsClass = new MenuSelection(["Salads"],
     "Breaded or grilled Mahi-mahi fresh romaine lettuce, tomato, red onions, croutons, cheese and diced advocado. <br> Served with our home made white sauce dressing.",
     "A crisp flour tortilla a shell filled with your choice of Grilled Chicken or Steak, onions, bell peppers, and tomato. <br> Topped with cheese sauce, lettuce and tomato."],
 ["* Tomatoes, sour cream, and pico de gallo are complementary upon request. * <hr width='25%'>"],
-["salads/tacoSalad.jpeg", "salads/grilledShrimpSalad.jpeg", "salads/chickenSalad.png", "salads/mahiSalad.jpeg", "salads/tacoSaladFajita.jpeg", "images/bucs.jpeg"] 
+["menuImage/salads/tacoSalad.jpeg", "menuImage/salads/grilledShrimpSalad.jpeg", "menuImage/salads/chickenSalad.png", "menuImage/salads/mahiSalad.jpeg", "menuImage/salads/tacoSaladFajita.jpeg", "images/bucs.jpeg"] 
 );
 
 const tacosClass = new MenuSelection(["Tacos"],
@@ -59,7 +59,7 @@ const tacosClass = new MenuSelection(["Tacos"],
     "Two grilled shrimp tacos topped with cheese & lettuce. <br> Served with rice, beans, and salsa.",
     "Three Mahi-Mahi tacos, breaded or grilled, topped with cabbage, red onions, avocado and our house made Salsa Rosada. <br> Try with chipotle aioli or white sauce."],
     ["Your choice of corn or flour tortillas. With Red salsa or Green mild salsa. Pico de gallo on request. <br> EXTRA Taco $4.50. <hr width='25%'>"],
-    ["tacos/carneAsadaTacos.jpeg", "tacos/carnitas-tacos.jpeg", "tacos/chorizo-tacos.jpeg", "tacos/al-pastor-tacos.jpeg", "tacos/pollo-asado-tacos.jpeg", "tacos/camaron-tacos.jpeg", "tacos/fish-tacos.jpeg"] 
+["menuImage/tacos/carneAsadaTacos.jpeg", "menuImage/tacos/carnitas-tacos.jpeg", "menuImage/tacos/chorizo-tacos.jpeg", "menuImage/tacos/al-pastor-tacos.jpeg", "menuImage/tacos/pollo-asado-tacos.jpeg", "menuImage/tacos/camaron-tacos.jpeg", "menuImage/tacos/fish-tacos.jpeg"] 
 );
 
 const burritoClass = new MenuSelection(["Burritos"],
@@ -74,7 +74,7 @@ const burritoClass = new MenuSelection(["Burritos"],
     "Burrito filled with ground beef or cooked chicken, covered with burrito sauce and topped with lettuce and tomato. <br> Served with rice and beans.",
     "Your choice of meat serving on a bowl: <div style='display: flex; flex-direction: row; justify-content: center; margin-left: 750px; margin-right: 750px;'> <div> <ul> <li>Fajita Steak</li><li>Fajita Chicken</li><li>Fajita cooked Pork</li><li>Grilled Steak</li><li>Grilled Chicken</li><li>Shredded Beef</li></ul> </div> <hr style='transform: rotate(0);'> <div> <b>Toppings:</b> <ul> <li>Rice</li> <li>Beans</li> <li> Lettuce </li> <li> Cheese </li> <li> Tomato </li> <li> Sour Cream </li> <li> Cheese Sauce </li> <li> Pico De Gallo </li> <li>Mild Sauce</li> <li>Hot Sauce </li></ul> </div> </div>"],
 ["* Your choice of burrito sauce or tomatillo green mild sauce. * <hr width='25%'>"],
-["burritos/burritos-norteños.jpeg", "burritos/don-burrito.png", "burritos/carnitas-burrito.jpeg", "burritos/deshebrada-burrito.jpg", "burritos/burrito-mexicanos.jpeg", "burritos/burrito-cancun.jpeg", "burritos/burrito-de-la-mesa.jpeg", "burritos/burrito-bowl.jpeg"]
+["menuImage/burritos/burritos-norteños.jpeg", "menuImage/burritos/don-burrito.png", "menuImage/burritos/carnitas-burrito.jpeg", "menuImage/burritos/deshebrada-burrito.jpg", "menuImage/burritos/burrito-mexicanos.jpeg", "menuImage/burritos/burrito-cancun.jpeg", "menuImage/burritos/burrito-de-la-mesa.jpeg", "menuImage/burritos/burrito-bowl.jpeg"]
 );
 
 const enchiladaClass = new MenuSelection(["Enchiladas"],
@@ -87,7 +87,7 @@ const enchiladaClass = new MenuSelection(["Enchiladas"],
     "Three chicken enchiladas topped with cheese. Served with rice and guacamole salad.",
     "Four enchiladas, one chicken, one beef, one cheese, and one bean. <br> Topped with cheese and lettuce."],
 ["* With Flour Tortilla <b class='flour-tortilla'>Add 99¢</b> * <hr width='25%'>"],
-["enchiladas/enchiladas-rojas.jpeg", "enchiladas/enchiladas-verdes.jpeg", "enchiladas/enchiladas-rancheras.jpeg", "enchiladas/enchiladas-de-la-mesa.jpeg", "enchiladas/enchiladas-de-pollo.jpeg", "enchiladas/enchiladas-mexicana.jpeg"]
+["menuImage/enchiladas/enchiladas-rojas.jpeg", "menuImage/enchiladas/enchiladas-verdes.jpeg", "menuImage/enchiladas/enchiladas-rancheras.jpeg", "menuImage/enchiladas/enchiladas-de-la-mesa.jpeg", "menuImage/enchiladas/enchiladas-de-pollo.jpeg", "menuImage/enchiladas/enchiladas-mexicana.jpeg"]
 );
 
 const quesadillasClass = new MenuSelection(["Quesadillas"],
@@ -95,7 +95,7 @@ const quesadillasClass = new MenuSelection(["Quesadillas"],
 [9.99, 10.99, 12.99],
 [" ", "", "Your choice of Grilled Chicken or Steak with sauteed onions, tomatoes, and bell peppers."],
 ["* Quesadillas served with lettuce, sour cream, and tomatoes. * <hr width='25%'>"],
-["quesadillas/cheese-quesadilla.jpeg", "quesadillas/chicken-quesadilla.jpeg", "quesadillas/fajita-quesadilla.jpeg"]
+["menuImage/quesadillas/cheese-quesadilla.jpeg", "menuImage/quesadillas/chicken-quesadilla.jpeg", "menuImage/quesadillas/fajita-quesadilla.jpeg"]
 );
 
 const seafoodClass = new MenuSelection(["Seafood"],
@@ -115,7 +115,7 @@ const seafoodClass = new MenuSelection(["Seafood"],
     "Tilapia seasoned and fried whole. <br> Served with rice, lettuce, and fresh avocado.",
     "Two 4oz Mahi-mahi steaks, seasoned and grilled with mushrooms, lemon juice and white wine. <br> Served on a bed of rice and smothered with cheese sauce. <br> Your choice offlour or corn tortillas."],
 [""],
-["seafood/seafoodVallarta.jpeg", "seafood/ceviche-de-camarones.jpeg", "seafood/Mazatlan.jpeg", "seafood/enchiladas-acapulco.jpeg", "seafood/coctel-de-camaron.jpeg", "seafood/quesadilla-de-camaron.jpeg", "seafood/enchiladas-cancun.jpeg", "seafood/camarones-la-diabla.jpeg", "seafood/los-cabos-shrimp.jpeg", "seafood/camarones-al-chipotle.jpeg", "seafood/shrimp-relleno.jpeg", "seafood/mojarra-frita.jpeg", "seafood/cozumel.jpeg"]
+["menuImage/seafood/seafoodVallarta.jpeg", "menuImage/seafood/ceviche-de-camarones.jpeg", "menuImage/seafood/Mazatlan.jpeg", "menuImage/seafood/enchiladas-acapulco.jpeg", "menuImage/seafood/coctel-de-camaron.jpeg", "menuImage/seafood/quesadilla-de-camaron.jpeg", "menuImage/seafood/enchiladas-cancun.jpeg", "menuImage/seafood/camarones-la-diabla.jpeg", "menuImage/seafood/los-cabos-shrimp.jpeg", "menuImage/seafood/camarones-al-chipotle.jpeg", "menuImage/seafood/shrimp-relleno.jpeg", "menuImage/seafood/mojarra-frita.jpeg", "menuImage/seafood/cozumel.jpeg"]
 );
 
 const fajitasClass = new MenuSelection(["Fajitas"],
@@ -129,7 +129,7 @@ const fajitasClass = new MenuSelection(["Fajitas"],
     "Our special hometown pork tips recipe.",
     "Shrimp and Mahi-mahi, spicy and grilled to perfection, onions, potatoes, zucchini, carrots, yellow squash, tomato, and mushrooms."],
 ["* Fajitas are grilled with onions, bell peppers, and tomatoes. <br> Served with a side of rice, beans, and lettuce. <br> <b class='fajita-side'>Add 1 additional side for $6</b> * <hr width='25%'>"],
-["fajitas/fajitas-rancheras.jpeg", "fajitas/fajitas-skillet.jpeg", "fajitas/fajitas-platter.jpeg", "fajitas/fajitas-shrimp-skillet.jpeg", "fajitas/fajitas-jalisco.jpeg", "fajitas/fajitas-de-carnitas.jpeg", "fajitas/fajitas-tulum.jpeg"]
+["menuImage/fajitas/fajitas-rancheras.jpeg", "menuImage/fajitas/fajitas-skillet.jpeg", "menuImage/fajitas/fajitas-platter.jpeg", "menuImage/fajitas/fajitas-shrimp-skillet.jpeg", "menuImage/fajitas/fajitas-jalisco.jpeg", "menuImage/fajitas/fajitas-de-carnitas.jpeg", "menuImage/fajitas/fajitas-tulum.jpeg"]
 );
 
 const dessertsClass = new MenuSelection(["Desserts"],
@@ -144,7 +144,7 @@ const dessertsClass = new MenuSelection(["Desserts"],
     "",
     "Three scoops of vanilla ice cream with your choice of strawberry or chocolate syrup topped with whipped cream and a cherry."],
 [""],
-["dessert/chocolate-cake.jpeg", "dessert/carrot-cake.jpeg", "dessert/cheese-cake.jpeg", "dessert/cheese-cake-chimichanga.jpeg", "dessert/flan.jpeg", "dessert/fried-ice-cream.jpeg", "dessert/ice-cream.jpeg", "dessert/sundae.jpeg"]
+["menuImage/dessert/chocolate-cake.jpeg", "menuImage/dessert/carrot-cake.jpeg", "menuImage/dessert/cheese-cake.jpeg", "menuImage/dessert/cheese-cake-chimichanga.jpeg", "menuImage/dessert/flan.jpeg", "menuImage/dessert/fried-ice-cream.jpeg", "menuImage/dessert/ice-cream.jpeg", "menuImage/dessert/sundae.jpeg"]
 );
 
 const nonAlcoholics = new MenuSelection(["Non-Alcholic Drinks"],
@@ -152,7 +152,7 @@ const nonAlcoholics = new MenuSelection(["Non-Alcholic Drinks"],
 [2.99, 2.99, 2.99, 2.99, 2.99, 2.99, 2.99, 2.99, 2.99],
 [" ", "", "", "", "", "", "", "", ""],
 [""],
-["non-alcoholic/pepsi.jpg", "non-alcoholic/dietPepsi.png", "non-alcoholic/pepsi-wild-cherry.jpg", "non-alcoholic/mountainDew.jpeg", "non-alcoholic/diet-mountainDew.jpeg", "non-alcoholic/root-beer.jpg", "non-alcoholic/dr-pepper.jpeg", "non-alcoholic/sierra-mist.jpeg", "non-alcoholic/tropicana-pink-lemonade.jpeg"]
+["menuImage/non-alcoholic/pepsi.jpg", "menuImage/non-alcoholic/dietPepsi.png", "menuImage/non-alcoholic/pepsi-wild-cherry.jpg", "menuImage/non-alcoholic/mountainDew.jpeg", "menuImage/non-alcoholic/diet-mountainDew.jpeg", "menuImage/non-alcoholic/root-beer.jpg", "menuImage/non-alcoholic/dr-pepper.jpeg", "menuImage/non-alcoholic/sierra-mist.jpeg", "menuImage/non-alcoholic/tropicana-pink-lemonade.jpeg"]
 );
 
 const alcoholics = new MenuSelection(["Alcholic Drinks"],
@@ -173,7 +173,7 @@ const alcoholics = new MenuSelection(["Alcholic Drinks"],
     "Margarita & Coronita.", 
     ""],
 ["* Monday & Tuesday 50% Off House <br> Ask About Our Premium Tequila Selection *"],
-["alcoholic/daiquiris.jpeg", "alcoholic/mojitos.jpeg", "alcoholic/pina-colada.jpeg", "alcoholic/beers-imported-on-tap.jpeg", "alcoholic/beers-imported.jpeg", "alcoholic/domestic-imported-beers.jpeg", "alcoholic/perfect-margarita.jpeg", "alcoholic/house-margarita.jpeg", "alcoholic/jalapeno.jpeg", "alcoholic/spicy-mango.jpeg", "alcoholic/cucumber.jpeg", "alcoholic/coronarita.jpeg", "alcoholic/tequila-sunrise.jpeg"]
+["menuImage/alcoholic/daiquiris.jpeg", "menuImage/alcoholic/mojitos.jpeg", "menuImage/alcoholic/pina-colada.jpeg", "menuImage/alcoholic/beers-imported-on-tap.jpeg", "menuImage/alcoholic/beers-imported.jpeg", "menuImage/alcoholic/domestic-imported-beers.jpeg", "menuImage/alcoholic/perfect-margarita.jpeg", "menuImage/alcoholic/house-margarita.jpeg", "menuImage/alcoholic/jalapeno.jpeg", "menuImage/alcoholic/spicy-mango.jpeg", "menuImage/alcoholic/cucumber.jpeg", "menuImage/alcoholic/coronarita.jpeg", "menuImage/alcoholic/tequila-sunrise.jpeg"]
 );
 
 var x = 1;
@@ -201,12 +201,14 @@ function updateQuantity(newX) {
 
 function updatePrice() {
     if (currentMenu) {
-        price.innerHTML = "$ " + currentMenu.prices[s] * x;
+        price.innerHTML = "$ " + (currentMenu.prices[s] * x).toFixed(2); // Fix to 2 decimal places
     }
 }
 
-plusButton.addEventListener('click', () => document.getElementById('output-area').innerHTML = ++x);
-minusButton.addEventListener('click', () => document.getElementById('output-area').innerHTML = --x);
+plusButton.addEventListener('click', () => updateQuantity(x + 1));
+minusButton.addEventListener('click', () => {
+    if (x > 1) updateQuantity(x - 1);
+});
 
 function advance(delta) {
     if (currentMenu) {
@@ -228,8 +230,6 @@ function setupMenu(menu) {
     advance(0);
 }
 
-plusButton.addEventListener('click', () => updateQuantity(++x));
-minusButton.addEventListener('click', () => updateQuantity(Math.max(--x, 1)));
 previousButton.addEventListener('click', () => advance(-1));
 nextButton.addEventListener('click', () => advance(1));
 
