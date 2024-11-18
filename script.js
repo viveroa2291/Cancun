2756ac6815dc25b3fc438c4859e71207190bd68c
viveroa2291 <viveroa2291@uwec.edu>
2024-11-17 23:11:23 -0600

    New Home Page
    New Home Page design with pagination to enhance UI .

--- script.js
+++ script.js
@@ -13,54 +13,26 @@ function toggleNav() {
         elements[i].classList.toggle('show');
     }
 }
-
-/* This is some TypeScript fun 
-// This is to test in the terminal. 
-let name = 0;
-let sami = "sami";
-const interestRate = 0.3;
-typeof name;
-console.log(name);
-console.log(interestRate);
-console.log(sami);
-console.log('This is a test, will delete soon. \ntoodles! \t !\n\n\n\n');
-
-// Object Literal
-let person = {
-    name: 'Mosh', 
-    age: 30
-};
-console.log(person);
-console.log(person.age);
-
-// Bracket Notation 
-person['name'] = 'Belinda Vivero';
-console.log(person.name);
-
-let selection = 'name';
-person[selection] = 'Mary';
-console.log(person.name);
-
-
-let selectedColors = ['red', 'blue'];
-console.log(selectedColors + "\n");
-selectedColors[2] = "green";
-
-console.log(selectedColors[0]);
-
-// Performing a task Functions 
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName + '.');
-}
-
-greet('Adan', 'Vivero');
-greet('Belinda', 'Vivero');
-
-// Calculate a value
-function square(number) {
-    return number * number;
-}
-
-let number = square(2);
-console.log(number);
-*/
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 100000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
